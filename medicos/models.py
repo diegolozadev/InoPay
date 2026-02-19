8dbb8fe66989b18ff2f3a066fb5842841900a62c
Diego Lozano <diegolozanodev@gmail.com>
2026-02-19 16:17:22 -0500

    Primer commit InoPay

---
+++ medicos/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Medico(models.Model):
+    nombre = models.CharField(max_length=100)
+    numero_documento = models.CharField(max_length=20, unique=True)
+    especialidad = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta:
+        verbose_name = "Medico"
+        verbose_name_plural = "Medicos"
+    
+    def __str__(self):
+        return f"{self.nombre} - {self.especialidad} {self.numero_documento}"
